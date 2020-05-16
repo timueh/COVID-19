65a95816fad08600f35a0d10cccdd9d6ab6d1b37
Tillmann Mühlpfordt <tillmann.muehlpfordt@kit.edu>
2020-05-16 18:54:08 +0200

    added further tests

--- test/rki-h.jl
+++ test/rki-h.jl
@@ -1,6 +1,6 @@
 using Test, ReproductionNumbers, ExcelFiles, DataFrames
 
-function get_rki_h_N_true(df::DataFrame; days_col::String="Erkrankungsdatum", cases_col::String="Neuberechnung mit Formel  NF(RKI-H)", R_col::String="Neuberechnung mit Formel R (RKI-H)")
+function get_reference_data(df::DataFrame; days_col::String, cases_col::String, R_col::String)
     days = df[!, days_col]
     
     cases = df[!, cases_col]
@@ -18,17 +18,24 @@ df = DataFrame(load("../data/20200511_Nowcasting_Zahlen.xlsx", "Nowcast_R"))
 cases, dates = df[!, "N(RKI-H)"], df[!, "Erkrankungsdatum"]
 df_cases = DataFrame(days = dates, cases = cases)
 
-k_gen = 4
+NAMES       = ["RKI-H"; "NEU-H"; "NEU-HA"]
+CASES_COL   = ["Neuberechnung mit Formel  NF(RKI-H)"; "NF(NEU-H)"; "NF(NEU-HA)"]
+R_COL       = ["Neuberechnung mit Formel R (RKI-H)"; "R(NEU-H)"; "R(NEU-HA)"]
+K_GEN       = [4; 4; 4]
+PAST        = K_GEN .- 1
+FUTURE      = [0; 3; 3]
+BUILD   = [build_R; build_R; build_R_acausal]
 
-rki_h_N_true, rki_h_R_true = get_rki_h_N_true(df)
-rki_h_N, rki_h_R = build_R(df_cases; past = k_gen - 1, future = 0, k_gen = k_gen)
+for (name, case_col, R_col, k_gen, past, future, build) in zip(NAMES, CASES_COL, R_COL, K_GEN, PAST, FUTURE, BUILD_FUN)
+    df_N_ref, df_R_ref = get_reference_data(df, days_col = "Erkrankungsdatum", cases_col = case_col, R_col = R_col)
+    df_N, df_R = build(df_cases; past = past, future = future, k_gen = k_gen)
+    @testset "$(name): Cases" begin
+        @test df_N_ref.days == df_N.days
+        @test round.(df_N_ref.cases) == round.(df_N.cases)
+    end
 
-@testset "RKI-H cases" begin
-    @test rki_h_N.days == rki_h_N_true.days
-    @test round.(rki_h_N.cases) == rki_h_N_true.cases
-end
-
-@testset "RKI-H R" begin
-    @test rki_h_R.days == rki_h_R_true.days
-    @test round.(rki_h_R.R, digits=2) == round.(rki_h_R_true.R, digits=2)
+    @testset "$(name): R" begin
+        @test df_R_ref.days == df_R.days
+        @test round.(df_R_ref.R, digits = 2) == round.(df_R.R, digits = 2)
+    end
 end
