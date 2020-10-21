af665c040ff98ce20e444d12d8d7ba0fb57ce698
benHeid <benHeid@users.noreply.github.com>
2020-10-21 22:03:21 +0200

    Prettify the app

--- app/app.jl
+++ app/app.jl
@@ -46,22 +46,59 @@ function get_layout()
 
     return html_div() do
         dcc_markdown("
-        # Covid-19 in Germany
+        # Point estimators for the reproduction number `R` During the COVID-19 Pandemic in Germany
         
-        ## Point estimators for the reproduction number `R`
+        Just like in most countries, the novel SARS-CoV-2 virus affects daily life in Germany.
+        It is not just the numerous deaths it has induced, but the so-called *new-normal* that everybody is still trying to adjust to.
+
+        Two indicators that often attract media attention are the
+        
+        - reproduction number `R` (i.e. the average number of people who get infected by a typical case),
+        - and the total number of cases.
+
+        The German Robert-Koch-Institut provides daily updates for both.
+
+        This dashboard compares the two methods used by the Robert Koch Institut to a recently proposed method.
+        The following table explains all methods, and all the different, publicly available data sources, from which you can choose.
         "),
-        # Just like in most countries, the novel SARS-CoV-2 virus affects daily life in Germany.
-        # It is not just the numerous deaths it has induced, but the so-called *new-normal* that everybody is still trying to adjust to.
+        html_h1(id = "my-h1"),
 
-        # Two indicators that often attract media attention are the
+
+        html_div([   
+            dcc_dropdown(
+            id = "data-source",
+            options=[
+                Dict("label" => "RKI nowcasting cases", "value" => "RKI-nowcasting"),
+                Dict("label" => "RKI reported cases", "value" => "RKI-reported"),
+            ],
+            value="RKI-nowcasting",
+            ),
+            html_p(id = "data-explanation")]
+        ),
         
-        # - reproduction number `R` (i.e. the average number of people who get infected by a typical case),
-        # - and the total number of cases.
-
-        # The German Robert-Koch-Institut provides daily updates for both.
-
-        # This dashboard compares the two methods used by the Robert Koch Institut to a recently proposed method.
-        # The following table explains all methods, and all the different, publicly available data sources, from which you can choose.
+        html_div([
+            html_div([
+                html_h3("R-values"),
+                dcc_graph(
+                    id = "R-values",
+                    figure = (
+                        data = data_R_rep,
+                        layout = layout_R_rep,
+                    )
+                ),
+            ], className="six columns"),
+    
+            html_div([
+                html_h3("N-values"),
+                dcc_graph(
+                    id = "N-values",
+                    figure = (
+                        data = data_N_rep,
+                        layout = layout_N_rep,
+                        )
+                )
+            ], className="six columns"),
+        ], className="row"),
         html_table(children = [
                     html_tr(children = [
                         html_th("Method"),
@@ -80,46 +117,8 @@ function get_layout()
                         html_td("Acausal estimator that accounts for three days of the past, the current day, and three days of the future; future values are based on the respective values from the previous week."),
                         ]),
         ]
-        ),
-        html_table(children = [
-                    html_tr(children = [
-                        html_th("Data source"),
-                        html_th("Explanation"),
-                    ]),
-                    html_tr(children = [
-                        html_td("RKI nowcasting cases"),  
-                        html_td("Nowcasting data on total number of cases provided by Robert Koch Institut"),  
-                        ]),
-                    html_tr(children = [
-                        html_td("RKI reported cases"),
-                        html_td("Full data set of reported cases, curated and updated daily by the Robert Koch Institut"),
-                        ]),
-        ]
-        ),
-        html_h1(id = "my-h1"),
-        dcc_dropdown(
-            id = "data-source",
-            options=[
-                Dict("label" => "RKI nowcasting cases", "value" => "RKI-nowcasting"),
-                Dict("label" => "RKI reported cases", "value" => "RKI-reported"),
-            ],
-            value="RKI-nowcasting",
-        ),
-        dcc_graph(
-            id = "R-values",
-            figure = (
-                data = data_R_rep,
-                layout = layout_R_rep,
-            )
-        ),
-        dcc_graph(
-            id = "N-values",
-            figure = (
-                data = data_N_rep,
-                layout = layout_N_rep,
-                )
         )
-        
+               
     end
 
 end
@@ -130,15 +129,16 @@ app = dash(external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 
 app.layout = get_layout
 
-callback!(app, [Output("R-values", "figure"), Output("N-values", "figure"), Output("my-h1", "children")], Input("data-source", "value")) do input_value
-    data_R, layout_R, data_N, layout_N = if input_value == "RKI-reported"
-        data_R_rep, layout_R_rep, data_N_rep, layout_N_rep
+callback!(app, [Output("R-values", "figure"), Output("N-values", "figure"), Output("my-h1", "children"), Output("data-explanation", "children")], Input("data-source", "value")) do input_value
+    data_R, layout_R, data_N, layout_N, explanation  = if input_value == "RKI-reported"
+        data_R_rep, layout_R_rep, data_N_rep, layout_N_rep, "Nowcasting data on total number of cases provided by Robert Koch Institut" 
+
     else
-        data_R_now, layout_R_now, data_N_now, layout_N_now
+        data_R_now, layout_R_now, data_N_now, layout_N_now, "Full data set of reported cases, curated and updated daily by the Robert Koch Institut"
     end
     R = round(data_R[3][:y][end], digits=2)
 
-    Dict(:data => data_R, :layout => layout_R), Dict(:data => data_N, :layout => layout_N), "R = $R"
+    Dict(:data => data_R, :layout => layout_R), Dict(:data => data_N, :layout => layout_N), "R = $R", "Explanation of the data source: $explanation"
 end
 
-run_server(app, "0.0.0.0", 8080)
+run_server(app, "0.0.0.0", parse(Int, ARGS[1]))
