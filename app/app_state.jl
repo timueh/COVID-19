bb2256c331559ad7e17efe8766400acbfcea42d8
Tillmann Mühlpfordt <tillmann.muehlpfordt@kit.edu>
2020-12-01 09:35:27 +0100

    #13, added R for Germany for comparison

--- app/app_state.jl
+++ app/app_state.jl
@@ -10,8 +10,17 @@ function get_data(path_to_file::String, title::String)
                 :name => value, ) 
                 for value in values ]
     layout = Dict("yaxis" => Dict("title" => title))
-    
+
     data, layout
+end
+
+function get_R_data_germany()
+    data, layout = get_data("../example/results-R-reported-Germany.csv", "R - Germany")
+
+    data_proj = data[end]
+    data_proj[:name] = "Germany"
+
+    data_proj
 end
 
 states = ["Baden-Württemberg",
@@ -31,16 +40,18 @@ states = ["Baden-Württemberg",
             "Schleswig-Holstein",
             "Thüringen"]
 
+country = "Germany"
+
 dropdown_opts = [ Dict("label"=>state, "value"=>state) for state in states]
 
 files_R  = "../example/results-R-reported-" .* states .* ".csv"
 files_N  = "../example/results-N-reported-" .* states .* ".csv"
 
-# R = Dict()
-# layout = Dict()
-# for (state, file_R) in zip(states, files_R)
-#     R[state], layout[state] = get_data(file_R, "R - "*state)
-# end
+R = Dict()
+layout = Dict()
+for (state, file_R) in zip(states, files_R)
+    R[state], layout[state] = get_data(file_R, "R - "*state)
+end
 
 app = dash(external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"])
 
@@ -59,7 +70,14 @@ end
 callback!(app, [Output("R-values", "figure"), Output("N-values", "figure"), Output("header", "children")], Input("data-source", "value")) do input_value
     R, l_R = get_data("../example/results-R-reported-"*input_value*".csv", "R - "*input_value)
     R_value = round(R[3][:y][end], digits=2)
-    N, l_N = get_data("../example/results-N-reported-"*input_value*".csv", "R - "*input_value)
+    N, l_N = get_data("../example/results-N-reported-"*input_value*".csv", "Cases - "*input_value)
+
+    R_ger, l_R_ger = get_data("../example/results-R-reported-Germany.csv", "R - Germany")
+    # N_ger, l_N_ger = get_data("../example/results-N-reported-Germany.csv", "Cases - Germany")
+
+    push!(R, get_R_data_germany())
+    # push!(N, N_ger[end])
+
     Dict(:data=>R, :layout=>l_R), Dict(:data=>N, :layout=>l_N), "R = $R_value"
 end
 
